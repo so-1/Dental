2febb62ece18acdacf8bfd8a32c9daa0fe7e371d
so-1 <24763955+so-1@users.noreply.github.com>
2018-03-13 22:34:50 +0900

    足場、ジャンプ操作、カメラ追随
    スクロールゲームの基本設定が完了

---
+++ New Unity Project 2/Assets/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //変数定義
+    public float flap = 10f;
+    public float scroll = 1f;
+    float direction = 0f;
+    Rigidbody2D rb2d;
+    bool jump = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //コンポーネント読み込み
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //キーボード操作
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1f;
+        }
+        else
+        {
+            direction = 0f;
+        }
+
+
+        //キャラのy軸のdirection方向にscrollの力をかける
+        rb2d.velocity = new Vector2(scroll * direction, rb2d.velocity.y);
+
+        //ジャンプ判定
+        if (Input.GetKeyDown("space") && !jump)
+        {
+            rb2d.AddForce(Vector2.up * flap);
+            jump = true;
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jump = false;
+        }
+        if (other.gameObject.CompareTag("Block"))
+        {
+            jump = false;
+        }
+    }
+}
+
